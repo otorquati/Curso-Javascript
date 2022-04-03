24000263f414a51dca8020a35c845e690508e102
Osvaldo <torquati.osvaldo@gmail.com>
2022-04-02 22:34:01 -0300

    Atualização  Ex. 14 - melhoria nas imagens e cores das DIVs

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -4,19 +4,19 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
     var minutos = data.getMinutes()
-    // var hora = 13
+    var hora = 1
     msg.innerHTML = `Agora são ${hora}:${minutos}`
     if (hora>=0 && hora < 12){
         // BOM DIA!
         img.src='fotomanha.png'
-        document.body.style.background = 'cian'
+        document.body.style.background = '#E2E12D'
     } else if(hora>=12 && hora<18){
         // BOA TARDE!
         img.src='fototarde.png'
-        document.body.style.background = 'red'
+        document.body.style.background = '#F4B07F'
     } else {
         // BOA NOITE!
         img.src='fotonoite.png'
-        document.body.style.background = 'gray'
+        document.body.style.background = '#3D1E38'
     }
 }
