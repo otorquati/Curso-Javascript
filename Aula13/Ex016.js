0fc104b0367bb6a31505b4c00961510326ef6a39
Osvaldo <torquati.osvaldo@gmail.com>
2022-04-06 19:57:30 -0300

    Aula 13 - Repetições

---
+++ Aula13/Ex016.js
@@ -0,0 +1,6 @@
+var c = 1
+var f =500
+while (c <= f) {
+    console.log(`Passo ${c}`)
+    c++
+} 
---
+++ Aula13/Ex017.js
@@ -0,0 +1,6 @@
+var c = 1
+var f =500
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= f)
