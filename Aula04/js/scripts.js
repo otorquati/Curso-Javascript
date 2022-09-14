2d734423c120f087cc61acd6753239c116480cf8
Osvaldo <torquati.osvaldo@gmail.com>
2022-09-13 21:49:48 -0300

    Aula 04 - Matheus Battisti
    Outro Curso de js

---
+++ Aula04/js/scripts.js
@@ -0,0 +1,7 @@
+var nome = "Osvaldo"
+var num = 2
+console.log(nome)
+produto = 'Flores' // Declaração Global - pode sobrescrever outra variável declara com var
+num+=2
+console.log(num)
+console.log(produto)
